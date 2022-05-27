597fa9a9fb0199bc71dd0f46d7be90f5e2a3f340
EthanKim <rlagmlruadle@icloud.com>
2022-05-27 14:48:08 +0900

    make color pick system

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
 canvas.height = 500;
 canvas.width = 500;
@@ -21,12 +22,17 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
+    ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.biginPath();
   } else {
     ctx.lineTo(x, y);
     ctx.stroke();
   }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -35,3 +41,5 @@ if (canvas) {
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach((color) => color.addEventListener('click', handleColorClick));
